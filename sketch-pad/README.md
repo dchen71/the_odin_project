bd3bb09f5557784d7c81165f3d7c37e7af81db3d
Dan <dchen12812@gmail.com>
2015-05-04 16:50:17 -0700

    update

---
+++ sketch-pad/README.md
@@ -0,0 +1,2 @@
+# Sketchpad
+Uses javascript/jQuery in order to create divs which change color when hovered over as well as reset to change box width/height
---
+++ sketch-pad/java.js
@@ -0,0 +1,9 @@
+//Javascript containing the code to create divs, change color on hover, and number of div boxes
+
+/*
+on ready - create container div
+start off with 3x3 grid
+on hover - change background color
+button - has prompt/resets and creates divs
+ */
+
