911616394b002ddc8574f5b6dc198742119d0ad3
Dan <dchen12812@gmail.com>
2015-05-05 01:51:47 -0700

    'update'

--- sketch-pad/java.js
+++ sketch-pad/java.js
@@ -7,3 +7,22 @@ on hover - change background color
 button - has prompt/resets and creates divs
  */
 
+//Initializes the grid numbers
+var row = 3;
+var col = 3;
+
+//Creates the container div
+//May have to create div and then do create class for container
+$(document).ready(function(){
+	$(body).append("<div class='container'></div>");
+});
+
+//Creates the initial set of 3x3 boxes
+$(document).ready(function(){
+	for (var i = 0; i <= row; i++) {
+		for (var i = 0; i <= col; i++) {
+			$(.container).append("<div></div>");
+		};
+	};
+});
+
