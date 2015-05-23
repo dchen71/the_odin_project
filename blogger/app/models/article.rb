1d6b3ad642d469e21a342b1678f556143f0fbf1b
Dan <dchen12812@gmail.com>
2015-05-22 18:22:41 -0700

    finished comments feature

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-
+	has_many :comments
 
 end
--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  
   get 'index' => 'articles#index'
   get 'show/:id' => 'articles#show'
 
