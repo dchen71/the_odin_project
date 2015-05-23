f3464cbb76d4db2e8a0b6059bde04d60b8123dec
Dan <dchen12812@gmail.com>
2015-05-22 22:40:16 -0700

    updated with clipper

--- blogger/app/controllers/articles_helper.rb
+++
@@ -1,9 +0,0 @@
-#Helper module to retreive title and body data from parameters
-
-module ArticlesHelper
-
-  def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
-  end
-
-end
--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523034323) do
+ActiveRecord::Schema.define(version: 20150523051159) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
